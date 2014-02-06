de5a8cf74c8bea6af10c768ae831c12d1a323f3e
max <msteing@gmail.com>
2014-02-06 16:10:15 -0500

    add garments controller

---
+++ app/controllers/garments_controller.rb
@@ -0,0 +1,7 @@
+class GarmentsController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 NewApp::Application.routes.draw do
+  get "garments/home"
+  get "garments/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
